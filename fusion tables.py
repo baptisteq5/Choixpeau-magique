4b2e6a6068843e3f049b400e413d500e3c74d899
Valentine <155440159+Valentine23012@users.noreply.github.com>
2024-01-16 14:50:00 +0100

    ajout de la partie calcul des distances

--- fusion tables.py
+++ fusion tables.py
@@ -1,6 +1,14 @@
 import csv
 
 def fusion_import_perso():
+    """
+    Import des tables "Characters.csv" et "Caracteristiques_des_persos.csv" et 
+    fusion de celles-ci
+    
+    Entrée : Rien
+    Sortie : une table de dictionnaires, résultat de la jointure des deux fichiers
+
+    """
     with open("Characters.csv", mode='r', encoding='utf-8') as f:
         reader = csv.DictReader(f, delimiter=';')
         characters_tab = [{key : value.replace('\xa0', ' ')
@@ -21,11 +29,44 @@ def fusion_import_perso():
     return poudlard_characters
 
 
-print(fusion_import_perso())
+# print(fusion_import_perso())
 
+def distancekppv(table, courage, ambition, intelligence, good):
+    """
+    Calcule la distance entre le personnage cible et tous les autres personnages
+    de Poudlard
+    
+    Entrée : une table de dictionnaire, 4 entiers correspondant aux caracteristiques 
+    du personnage cible
+    
+    Sortie : une table de dictionnaire regroupant la distance entre le personnage
+    cible et chaque personnage de poudlard
 
-
-
+    """
+    table_distance = []
+    for prenom in table:
+        dico_distances = {}
+        
+        # changer les caractéristiques des personnages en entiers
+        courage_perso = int(prenom["Courage"])
+        ambition_perso = int(prenom["Ambition"])
+        intelligence_perso = int(prenom["Intelligence"])
+        good_perso = int(prenom["Good"])
+        
+        distance = ((courage - courage_perso)**2 + 
+                    (ambition - ambition_perso)**2 +
+                    (intelligence - intelligence_perso)**2 +
+                    (good - good_perso)**2)**0.5
+        
+        dico_distances["Prenom"] = prenom["Name"]
+        dico_distances["Distance"] = distance
+        table_distance.append(dico_distances)
+        table_distance.sort(key=lambda x: x["Distance"] )
+    return table_distance
+    
+    
+poudlard_characters = fusion_import_perso()   
+print(distancekppv(poudlard_characters, 5, 3, 8, 9))
 
 
 
