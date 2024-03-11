3f2d514baf41ba638a76fee7eea06aad21c862f5
Valentine <155440159+Valentine23012@users.noreply.github.com>
2024-03-11 19:38:46 +0100

    code brython version 2

--- brython.py
+++ brython.py
@@ -1,8 +1,46 @@
 from browser import document, html
 
-document <= html.B("Hello World ! ")
+table_questions = []
+with open("questionnaire version 1.csv", mode='r', encoding='utf-8') as f:
+    lines = f.readlines()
+    key_line = lines[0].strip()
+    keys = key_line.split(";")
+    for line in lines[1:]:
+        line = line.strip()
+        values = line.split(';')
+        dico = {}
+        for i in range(len(keys)):
+            dico[keys[i]] = values[i]
+        table_questions.append(dico)
+print(table_questions)  
 
+i = 0
 def change_texte(ev):
-    document["button"].textContent = "Nouvelle réponse"
+    global i
+    document["bouton1"].textContent = table_questions[i]["Reponse 1"]
+    document["bouton2"].textContent = table_questions[i]["Reponse 2"]
+    document["bouton3"].textContent = table_questions[i]["Reponse 3"]
+    document["zone_question"].textContent = table_questions[i]["Question"]
+    i += 1
+    
 
-document["button"].bind("click", change_texte)
+profil = {'Courage' : 0, 'Ambition' : 0, 'Intelligence' : 0, 'Good' : 0}
+
+def profil_bouton_1(profil_perso, table_questions):
+    for valeur in table_questions["Profil"]:
+        int(valeur)
+        valeur.split(",")
+        print(valeur)
+        """
+        for cate in profil.values:
+            cate += valeur
+    print(profil)
+    """
+    
+
+
+
+
+document["bouton1"].bind("click", change_texte)
+document["bouton2"].bind("click", change_texte)
+document["bouton3"].bind("click", change_texte)
